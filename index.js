117ef7dd6b5d50834b6ac6081bed86062da9cd70
Kristina Rudyte <rkristina32@yahoo.com>
2022-03-29 13:05:26 +0300

    training

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-console.log(21456);
-console.log(21190);
-console.log(324234);
+const vardas = 'Maryte';
+console.log('Laba diena, Petras.');
+console.log('Laba diena, '+ vardas + '.');
+const pasisveikinimas = 'Laba diena, '+ vardas + '.';
+console.log(pasisveikinimas);
+
+let pinigine = 0;
+pinigine = 5;
+console.log(pinigine);
+
+pinigine = 10;
+console.log(pinigine);
+
+const arVedes = true;
+const arTuriVaiku = false;
+
+const arMeluojaDelVaiku = true;
+console.log('Ar vedes?', arVedes);
+console.log('Ar turi vaiku?', arTuriVaiku);
+const pazymiai = [10, 2, 8, 4, 6];
+console.log('pazymiai', pazymiai);
+
+const booleanList = [true, false, true, false, true, false];
+console.log(booleanList);
