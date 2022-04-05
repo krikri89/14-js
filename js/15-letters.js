32c187aef0ac73bd1497b42a742e0ac1a5f6464a
Kristina Rudyte <rkristina32@yahoo.com>
2022-04-05 15:13:38 +0300

    fixed

--- js/15-letters.js
+++ js/15-letters.js
@@ -44,28 +44,34 @@ function letters(text, step) {
   if (step > text.length){
       return 'ERROR: zingsnis negali buti lygus nuliui'
   }
-let raides = ``
-for (let i=step -1; i < text.length; i+= step){
-    const raide = text[i];
-    raides += raide;
+let raides = '';
+
+if (step > 0) {
+    for (let i=step - 1; i < text.length; i+= step){
+    raides = text[i];
+    }
+} else {
+  for (let i = text.length + step; i >= 0; i += step){
+    raides += text[i];
+  }
 }
   return raides;
 }
 
-console.log(letters(1561, 2), '-->', 'ERROR');
-console.log(letters('labas', 'iki'), '-->', 'ERROR');
-console.log(letters('labas', NaN), '-->', 'ERROR');
-console.log(letters('labas', -Infinity), '-->', 'ERROR');
-console.log(letters('labas', Infinity), '-->', 'ERROR');
-console.log(letters('labas', 1.111111), '-->', 'ERROR');
-console.log(letters('abc', 0), '-->', 'ERROR');
-console.log(letters('abc', 4), '-->', 'ERROR');
-console.log(letters('', 4), '-->', 'ERROR');
+// console.log(letters(1561, 2), '-->', 'ERROR');
+// console.log(letters('labas', 'iki'), '-->', 'ERROR');
+// console.log(letters('labas', NaN), '-->', 'ERROR');
+// console.log(letters('labas', -Infinity), '-->', 'ERROR');
+// console.log(letters('labas', Infinity), '-->', 'ERROR');
+// console.log(letters('labas', 1.111111), '-->', 'ERROR');
+// console.log(letters('abc', 0), '-->', 'ERROR');
+// console.log(letters('abc', 4), '-->', 'ERROR');
+// console.log(letters('', 4), '-->', 'ERROR');
 
 console.log(letters('abcdefg', 2), '-->', 'bdf');
 console.log(letters('abcdefghijkl', 3), '-->', 'cfil');
 console.log(letters('abcdefghijkl', 4), '-->', 'dhl');
 
-console.log(letters('abcdefg', -2), '-->', 'fdb');
-console.log(letters('abcdefghijkl', -3), '-->', 'jgda');
-console.log(letters('abcdefghijkl', -4), '-->', 'iea');
+// console.log(letters('abcdefg', -2), '-->', 'fdb');
+// console.log(letters('abcdefghijkl', -3), '-->', 'jgda');
+// console.log(letters('abcdefghijkl', -4), '-->', 'iea');
