a537254a62105696f5f673f12f7cb19efcf0c62d
Kristina Rudyte <rkristina32@yahoo.com>
2022-04-06 17:10:37 +0300

    classes

--- js/17-class.js
+++ js/17-class.js
@@ -8,72 +8,87 @@ Objektas - pagal receptura pagamintas konkretus/apciuopamas dalykas;
 */
 
 class Bankomatas {
-  constructor(a, b) {
-    this.spalva = a;
-    this.gatve = b;
+  constructor(street, color) {
+    this.color = color;
+    this.street = street;
     this.money = 0;
     this.turnedOn = false;
   }
+
   switchOn() {
     this.turnedOn = true;
-    console.log(`${this.street}, bankomatas veikia`);
-}
-switchOff() {
-    this.turnedOn = false;
-    console.log(`${this.street}, bankomatas neveikia`);
+    console.log(`${this.street}: bankomatas veikia`);
   }
+  switchOff() {
+    this.turnedOn = false;
+    console.log(`${this.street}: bankomatas neveikia`);
+  }
+
   cashIn(money) {
     // jei neijungtas - Error
     //jei ne normalus - Error
     //isskaiciuojama pinigus
 
     if (!this.turnedOn) {
-      console.log("Panasu kad nera elektros..");
-      return
+      console.log(this.street, "Panasu kad nera elektros..");
+      return;
     }
     if (typeof money !== "number" || !isFinite(money)) {
-      console.log("fake money");
-      return
+      console.log(this.street, "fake money");
+      return;
     }
     if (money <= 0) {
-      console.log("minusiniu pinigu neegzistuoja");
-      return
+      console.log(this.street, "minusiniu pinigu neegzistuoja");
+      return;
     }
+
     this.money += money;
     console.log(`${this.street}: inesta ${money} eur`);
   }
+
   cashOut(money) {
     if (!this.turnedOn) {
-        console.log("Panasu kad nera elektros..");
-        return
+      console.log(this.street, "Panasu kad nera elektros..");
+      return;
+    }
+    if (typeof money !== "number" 
+    || !isFinite(money)) {
+      console.log(this.street, "fake money");
+      return;
+    }
+    if (money <= 0) {
+      console.log(this.street, "minusiniu pinigu neegzistuoja");
+      return;
+    }
+    if (this.money < money) {
+      console.log(this.street, "no money, sorry");
+      return;
+    }
+    this.money -= money;
+    console.log(`${this.street}: isgryninta ${money} eur`);
   }
-  if (typeof money !== "number" || !isFinite(money)) {
-    console.log("fake money");
-    return
+  status () {
+      console.log(`${this.street}: siuo metu pinigu likutis yra ${this.money}`);
   }
-  if (money <= 0) {
-    console.log("minusiniu pinigu neegzistuoja");
-    return
-  }
-  if (this.money < money) {
-      console.log('no money, sorry');
-  }
-  this.money -= money;
-  console.log(`${this.street}: isgryninta ${money} eur`);
 }
-}
+
 const bankomatas1 = new Bankomatas("Didzioji g. 2", "red");
 const bankomatas2 = new Bankomatas("Mazoji g. 3", "blue");
 
-// bankomatas1.switchOn;
-// bankomatas2.switchOn;
-// bankomatas1.switchOff;
-// bankomatas2.switchOff;
-
-// console.log(bankomatas1);
-// console.log(bankomatas2);
 bankomatas1.switchOn();
-bankomatas1.cashIn(5);
-bankomatas1.cashOut(9);
+bankomatas2.switchOn();
 
-console.log(bankomatas1.money);
+bankomatas1.cashIn(true);
+bankomatas1.cashIn(5);
+bankomatas2.cashIn(777);
+bankomatas1.cashIn(5);
+bankomatas1.cashIn(5);
+bankomatas2.cashIn(3);
+
+bankomatas1.cashOut(9);
+bankomatas2.cashOut(9);
+bankomatas1.cashOut(9);
+bankomatas2.cashOut(9);
+
+bankomatas1.status();
+bankomatas2.status();
